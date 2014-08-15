dec688c15eeab489216116cd0f19d49b1569c049
Root <sam.j.vitello@gmail.com>
2014-08-14 19:56:09 -0700

    Created Deck and Hand Manager Scripts

---
+++ ChessStone/Assets/Scripts/Unit/DeckManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DeckManager : MonoBehaviour {
+
+	public Card[] player_deck = new Card[30];
+	public int deck_count = 0;
+	// Use this for initialization
+	public void add(Card card){
+		if (deck_count < 30){
+			player_deck[deck_count]=card;
+			deck_count++;
+		}
+	}
+
+	public Card nextCard(){
+		Card temp = player_deck [deck_count];
+		deck_count--;
+		return temp;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ChessStone/Assets/Scripts/Unit/HandManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using X_UniTMX;
+
+public class HandManager : MonoBehaviour {
+
+	//Hand Array Containing Card objects
+	//Has an extra node in position 8 that always contains Null
+	//	for the playCard() function to set empty positions in Array 
+	private Card[] hand = new Card[8]; 
+	public int deck_pos = 0;
+	void Start () {
+		}
+	
+	// Update is called once per frame
+	public void add(Card card){
+		if (deck_pos < 7) {
+			hand[deck_pos] = card;
+			deck_pos++;
+			//update GUI
+		}
+
+	}
+
+	public void playCard(int index){
+		deck_pos --;
+		Card toRet = hand[index];
+		hand [index] = null;
+		for (int i = index; i < hand.Length-1; i++) {
+			Card temp = hand[i+1];
+			hand [i] = temp;
+		}
+		//RefreshGUI();
+		//Run Card Playing Script
+		toRet.run();
+	}
+}
